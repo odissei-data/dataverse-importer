a05df738b014203c87326bae5a1fa5ba1473ebf7
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MacBook-Pro.local>
2022-11-22 13:46:53 +0100

    Removed unused import.

--- src/main.py
+++ src/main.py
@@ -2,8 +2,6 @@ import json
 
 from fastapi import FastAPI
 from pyDataverse.api import NativeApi
-from pyDataverse.models import Dataset
-from pyDataverse.utils import read_file
 
 from schema.input import ImporterInput
 from version import get_version
@@ -18,16 +16,11 @@ async def info():
 
 
 @app.post("/importer")
-def map_metadata(importer_input: ImporterInput):
-
-    ds = Dataset()
-    filename = "output.json"
-    with open(filename, 'w') as outfile:
-        json.dump(importer_input.metadata, outfile)
-    ds.from_json(read_file(filename))
+def import_metadata(importer_input: ImporterInput):
+    metadata = json.dumps(importer_input.metadata)
     api = NativeApi(importer_input.dataverse_information.base_url,
                     importer_input.dataverse_information.api_token)
     response = api.create_dataset(
         importer_input.dataverse_information.dt_alias,
-        ds.json())
+        metadata)
     return response.json()
