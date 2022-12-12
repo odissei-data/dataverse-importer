8e18c634f5d13639e89186a366b48b50d0421793
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MBP.ia.knaw.local>
2022-12-12 11:29:58 +0100

    Removed forward slash from example url.

--- src/schema/input.py
+++ src/schema/input.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field
 
 
 class DataverseInformation(BaseModel):
-    base_url: str = Field(example="https://portal.odissei.nl/")
+    base_url: str = Field(example="https://portal.odissei.nl")
     dt_alias: str = Field(example="dans-easy")
     api_token: str = Field(example="12345678-ab12-12ab-abcd-a1b2c3d4e5g6")
 
