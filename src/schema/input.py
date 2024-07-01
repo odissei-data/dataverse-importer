752d06faa441f22571b13eaa2e3c537c430d30b8
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MBP.ia.knaw.local>
2024-07-01 14:17:36 +0200

    Improved schema.

--- src/schema/input.py
+++ src/schema/input.py
@@ -3,8 +3,8 @@ from pydantic import BaseModel, Field
 
 
 class DataverseInformation(BaseModel):
-    base_url: str = Field(example="https://portal.odissei.nl")
-    dt_alias: str = Field(example="dans-easy")
+    base_url: str = Field(example="https://portal.staging.odissei.nl")
+    dt_alias: str = Field(example="dans")
     api_token: str = Field(example="12345678-ab12-12ab-abcd-a1b2c3d4e5g6")
 
 
